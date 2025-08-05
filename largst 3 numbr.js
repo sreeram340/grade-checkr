513c7e064d37e7ab7ebf6e74692dd512e41653ab
sreeram340 <ssreeram63@gmail.com>
2025-08-06 01:22:22 +0530

    Add files via upload

---
+++ largst 3 numbr.js
@@ -0,0 +1,11 @@
+const num1 = 90;
+const num2 = 50;
+const num3 = 80;
+
+if (num1 > num2 && num1 > num3) {
+  console.log(num1 + " is largest");
+} else if (num2 > num1 && num2 > num3) {
+  console.log(num2 + " is largest");
+} else {
+  console.log(num3 + " is largest");
+}
