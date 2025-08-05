aa8b5cb31368021b73e6075b81256a768afd604a
sreeram340 <ssreeram63@gmail.com>
2025-08-06 01:14:20 +0530

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+const mark=101;
+
+
+if(mark>=90 && mark<=100){
+    console.log("Grade A")
+}else if(mark>=75 && mark<=89){
+    console.log("Grade B")
+}if (mark >= 90 && mark <= 100) {
+  console.log("Grade A");
+} else if (mark >= 75 && mark <= 89) {
+  console.log("Grade B");
+} else if (mark >= 50 && mark <= 74) {
+  console.log("Grade C");
+} else if (mark >= 0 && mark <= 49) {
+  console.log("Fail");
+} else {
+  console.log("Invalid");
+}
