45d0993780f46825998f54f5753df0488ad27d75
sreeram340 <ssreeram63@gmail.com>
2025-08-06 01:20:42 +0530

    Add files via upload

---
+++ num sign chekr.js
@@ -0,0 +1,8 @@
+const numb = 0;
+if (numb > 0) {
+  console.log("The number is positive");
+} else if (numb < 0) {
+  console.log("The number is negative");
+} else {
+  console.log("the Number is zero");
+}
